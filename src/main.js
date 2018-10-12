8f5d9e50a75ed1bac508f12ad4199104027f8b59
jramer <jramer87@gmail.com>
2018-10-12 16:32:15 -0500

    Binds genres to check-filter component

--- src/main.js
+++ src/main.js
@@ -1,18 +1,48 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
 
 new Vue({
     el: '#app',
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                        Movie List
-                       </div>`
+                            <div v-for="movie in movies" class="movie">
+                                {{ movie.title }}
+                            </div>
+                       </div>`,
+            data() {
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Home Alone' },
+                        { title: 'Austin Powers' }
+                    ]
+                };
+            }
         },
         'movie-filter': {
+            data() {
+                return {
+                    genres
+                }
+            },
             template: `<div id="movie-filter">
-                        Movie Filter
-                       </div>`
+                            <h2>Filter Results</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres" v-bind:genre="genre"></check-filter>
+                            </div>
+                       </div>`,
+            components: {
+                'check-filter': {
+                    props: [ 'genre' ],
+                    template: `<div class="check-filter">
+                                    <span class="checkbox"></span>
+                                    <span class="check-filter-title"></span>
+                                    {{ genre }}
+                              </div>'`
+                }
+            }
         }
     }
 });
