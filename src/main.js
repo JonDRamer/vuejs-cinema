7dbb5e2503a20085f319b0ec8cd06a711b9a1c19
jramer <jramer87@gmail.com>
2018-11-28 15:32:21 -0600

    Adds genre filter

--- src/main.js
+++ src/main.js
@@ -21,33 +21,43 @@ new Vue({
         }
     },
     components: {
-        'movie-list': {
-            template: `<div id="movie-list">
-                            <div v-for="movie in filteredMovies" class="movie">
-                                {{ movie.title }}
-                            </div>
-                       </div>`,
-            data() {
-                return {
-                    movies: [
-                        { title: 'Pulp Fiction', genre: genres.CRIME },
-                        { title: 'Home Alone', genre: genres.COMEDY },
-                        { title: 'Austin Powers', genre: genres.COMEDY }
-                    ]
-                };
-            },
-            props: [ 'genre', 'time' ],
-            methods: {
-                moviePassesGenreFilter(movie) {
-                    this.genre.find(genre => movie.genre === genre)
-                }
-            },
-            computed: {
-                filteredMovies() {
-                    return this.movies.filter(this.moviePassesGenreFilter);
-                }
-            }
-        },
+       'movie-list': {
+           template: `<div id="movie-list">
+                    <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+                </div>`,
+           data() {
+               return {
+                   movies: [{
+                           title: 'Pulp Fiction',
+                           genre: genres.CRIME
+                       },
+                       {
+                           title: 'Home Alone',
+                           genre: genres.COMEDY
+                       },
+                       {
+                           title: 'Austin Powers',
+                           genre: genres.COMEDY
+                       }
+                   ]
+               }
+           },
+           props: ['genre', 'time'],
+           methods: {
+               moviePassesGenreFilter(movie) {
+                   if (!this.genre.length) {
+                       return true;
+                   } else {
+                       return this.genre.find(genre => movie.genre === genre);
+                   }
+               }
+           },
+           computed: {
+               filteredMovies() {
+                   return this.movies.filter(this.moviePassesGenreFilter);
+               }
+           }
+       },
         'movie-filter': {
             data() {
                 return {
